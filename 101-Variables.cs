b932233a880e3cd250d3d8bfc9001dfb764d7a26
jeerawut <jeerawut.jd40@gmail.com>
2024-07-30 11:29:46 +0700

    101 Variables

---
+++ 101-Variables.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            var numberVar = 2;
+            var countVar = 10;
+            var totalPriceVar = 20.95f;
+            var characterVar = 'A';
+            var firstNameVar = "Mosh";
+            var isWorkingVar = false;
+
+            Console.WriteLine(numberVar);
+            Console.WriteLine(countVar);
+            Console.WriteLine(totalPriceVar);
+            Console.WriteLine(characterVar);
+            Console.WriteLine(firstNameVar);
+            Console.WriteLine(isWorkingVar);
+        }
+    }
+}
